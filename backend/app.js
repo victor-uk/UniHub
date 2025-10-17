86af72f8820d36dd7211fce12c51a2648f62ceaf
Victor Ukaegbu <ikwuonwuvictor@gmail.com>
2025-10-17 18:43:18 +0100

    Update CORS configuration to include production frontend URL for socket connections

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ initSocket(server);
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','https://uni-hub-ivory.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
--- backend/services/socketService.js
+++ backend/services/socketService.js
@@ -5,7 +5,7 @@ let io;
 export const initSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"], // Your frontend URL
+            origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", 'https://uni-hub-ivory.vercel.app'], // Your frontend URL
             methods: ["GET", "POST"]
         }
     });
